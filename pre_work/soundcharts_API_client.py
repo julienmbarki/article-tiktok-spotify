4a88b5046bbb237f5c9cb429c2dfb7782dc72af5
Julien M'Barki <julienmbarki@yahoo.com>
2024-11-21 18:04:16 +0100

    Souncharts client update + starting requests

--- pre_work/soundcharts_API_client.py
+++ pre_work/soundcharts_API_client.py
@@ -16,14 +16,14 @@ class SoundchartsClient(object):
     def get_resource(self, endpoint, params=None, data=None):
         url = f"{self.base_url}{endpoint}"
 
-        response = requests.request(url, headers=self.headers, params=params, json=data)
+        response = requests.get(url, headers=self.headers, params=params)
         
         if response.status_code not in range(200,299):
             return{}
         return response.json()
     
     # Get TikTok's top charts 
-    def get_tiktok_charts(self, offset=0, limit=100, date='date'):
+    def get_tiktok_charts(self, offset=0, limit=100, date=''):
         endpoint = f"v2/chart/tiktok/music/weekly/ranking/{date}"
 
         params = {
@@ -34,7 +34,7 @@ class SoundchartsClient(object):
         return self.get_resource(endpoint, params=params)
     
     # Get track's TikTok streams by date
-    def get_tiktok_streams_by_date(self, endDate='', period=90, track_id='track_id'):
+    def get_tiktok_streams_by_date(self, endDate='', period=90, track_id=''):
         endpoint = f"v2/tiktok/music/{track_id}/video/volume"
 
         params = {
@@ -43,9 +43,9 @@ class SoundchartsClient(object):
         }
 
         return self.get_resource(endpoint, params=params)
-    
+
     # Get track's Spotify streams by date
-    def get_spotify_streams_by_date(self, startDate='', endDate='', track_id='track_id'):
+    def get_track_streams_by_date(self, startDate='', endDate='', track_id=''):
         endpoint = f"v2/song/{track_id}/audience/spotify"
 
         params = {
@@ -57,25 +57,12 @@ class SoundchartsClient(object):
     
     # Get track's metadata
     def get_track_metadata(self, track_id='track_id'):
-        endpoint = f"/api/v2.25/song/{track_id}"
+        endpoint = f"v2.25/song/{track_id}"
 
         return self.get_resource(endpoint)
-    
-    # Get artist's first track date
-    def get_first_track_date(self, offset=0, limit=50, sortBy='releaseDate', sortOrder='asc', artist_id='artist_id'):
-        endpoint = f"v2.21/artist/{artist_id}/songs"
-
-        params = {
-            'offset': offset,
-            'limit': limit,
-            'sortBy': sortBy,
-            'sortOrder': sortOrder
-        }
-
-        return self.get_resource(endpoint, params=params)
 
     # Get artist's Spotify streams by date
-    def get_streams_by_date(self, startDate='', endDate='', artist_id='artist_id'):
+    def get_artist_streams_by_date(self, startDate='', endDate='', artist_id=''):
         endpoint = f"v2/artist/{artist_id}/streaming/spotify/listening"
 
         params = {
